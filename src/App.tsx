d4545bafc73a6a1bdb0a448c5b100ef43a0ff89b
T_DaMER <dpancov64@gmail.com>
2024-10-25 17:55:45 +0300

    100dvh

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,14 @@ import { ErrorBoundary } from 'react-error-boundary'
 import { Route, Router, Switch } from 'wouter-preact'
 import { ToastContainer } from 'react-toastify'
 import { useHashLocation } from 'wouter-preact/use-hash-location'
-import Button from 'components/Button'
 import DetailsPage from 'screens/DetailsPage'
+import ErrorFallback from 'components/ErrorFallback'
 import Footer from 'components/Footer'
 import Main from 'screens/Main'
-import goMain from 'helpers/goMain'
-
-function ErrorFallback() {
-  return (
-    <div className="flex flex-col gap-y-2 items-center justify-center w-full h-screen">
-      <span>Что-то сломалось :(</span>
-      <Button
-        onClick={() => {
-          goMain()
-          window.location.reload()
-        }}
-      >
-        Вернуться на главную
-      </Button>
-    </div>
-  )
-}
 
 export default function () {
   return (
-    <div className="flex flex-col container min-h-screen mx-auto p-5 md:p-10 prose">
+    <div className="flex flex-col container min-h-[100dvh] mx-auto p-5 md:p-10 prose">
       <Router hook={useHashLocation}>
         <Switch>
           <ErrorBoundary fallback={<ErrorFallback />}>
